daeb774bf81355d34ef2852ac921fbc1c2e518c2
AbiTravers <abigail.travers@bglgroup.com>
2018-02-28 19:18:15 +0000

    Setting up how authetication helper will be called in prgramme.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace AuthenticatorPractice
         AuthenticationHelper aOuth = new AuthenticationHelper("mozscape - 45676ffgg11918", "5dd5afgwash556as786fc2ce94977b1db23c");
          var accessTime1 = aOuth.CreateAllowedAccessTime();
             Console.WriteLine(accessTime1);
-        //var hmacHashString = aOuth.HmacSHA1HashString();
+
+         var encryptedSignature = aOuth.CreateEncriptedSignature(accessTime1);
+         var authenticationStr = aOuth.GetAuthenticationStr(encryptedSignature, accessTime1);
+
+
+            //var hmacHashString = aOuth.HmacSHA1HashString();
             //Console.WriteLine(hmacHashString);
 
             Console.ReadKey();
